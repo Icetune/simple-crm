ec66a642d3abdf0f35ea4b0b3083b5c3f2ebb344
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-08 14:48:31 +0100

    25 done

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent {
+
+  userID = '';
+
+  constructor(private route:ActivatedRoute) {
+
+    this.route.paramMap.subscribe( paramMap => {
+      this.userID = paramMap.get('id');
+      console.log('GOT ID:', this.userID);
+      
+    })
+
+  }
 
 }
