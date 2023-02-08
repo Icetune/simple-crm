42c57e1a4533872b0c40ee19f867af39b2200e7a
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-08 14:01:35 +0100

    22 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,13 +29,15 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     UserComponent,
-    DialogAddUserComponent
+    DialogAddUserComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
