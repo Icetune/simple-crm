c930d8196f0b4da343bad148846f0bcd679974c7
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-08 15:02:20 +0100

    26 done

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -9,15 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent {
 
   userID = '';
+  user: User = new User;
 
-  constructor(private route:ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore) {
 
-    this.route.paramMap.subscribe( paramMap => {
+    this.route.paramMap.subscribe(paramMap => {
       this.userID = paramMap.get('id');
       console.log('GOT ID:', this.userID);
-      
+      this.getUser();
     })
+  }
 
+
+  getUser() {
+    this
+      .firestore
+      .collection('users')
+      .doc(this.userID)
+      .valueChanges()
+      .subscribe((user: User) => {
+        this.user = new User (user);
+        console.log('Retrieved user', this.user);
+      })
   }
 
 }
